64fe15789b05a354a3e6bb38a2a589a891dd08ad
ABDUL RAHMAN MIHAD C M <mihadcm@gmail.com>
2025-05-14 20:18:33 +0530

    updated admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from orders.models import Order, OrderedItem
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = [
+        'owner',
+        'order_status',
+        'created_at',
+        'delete_status'
+    ]
+    search_fields = [
+        'owner__user__username',
+        'id',
+        'order_status',
+        'created_at',
+        'updated_at',
+        'delete_status'
+    ]
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderedItem)
 
 # Register your models here.
